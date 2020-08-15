08b50d76484cc124b7e56b106395e2877f6ad48e
Lucas Zapico <lucaszapico@luca-macbook.local>
2020-08-15 15:52:50 -0700

    add backgorund img and icon

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,12 +7,14 @@ const HomePage = () => {
     
     return ( 
     <div className="page--home container--full">
+        
         {quotes.length > 0 && <div className="home--quote">
           <h3> { quotes[0].quote}</h3>
           <h5>- {quotes[0].author}</h5>
           <div>{quotes[0].like}</div>
        </div>
        }
+       <div className="home--background"></div>
     </div>
     );
     }
